5f2876d3ed801443d60690d099614ac4b3e4875d
Your Name <you@example.com>
2016-04-28 20:29:51 +0800

    test

---
+++ APP-ServerAI/src/com/app/server/atavism/agis/plugins/CombatClient.java
@@ -0,0 +1,28 @@
+// 
+// Decompiled by Procyon v0.5.30
+// 
+
+package com.app.server.atavism.agis.plugins;
+
+import com.app.server.atavism.server.messages.PropertyMessage;
+import com.app.server.atavism.agis.core.AgisAbility;
+import com.app.server.atavism.agis.core.Cooldown;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.LinkedList;
+import com.app.server.atavism.server.plugins.WorldManagerClient;
+import java.util.HashMap;
+import java.util.List;
+import com.app.server.atavism.server.objects.ObjectType;
+import com.app.server.atavism.server.math.Point;
+import java.io.Serializable;
+import com.app.server.atavism.server.engine.Engine;
+import com.app.server.atavism.server.engine.OID;
+import com.app.server.atavism.server.engine.Namespace;
+
+public class CombatClient {
+	public static Namespace NAMESPACE = null;
+	public static Namespace TEST_NAMESPACE = null;
+
+}
