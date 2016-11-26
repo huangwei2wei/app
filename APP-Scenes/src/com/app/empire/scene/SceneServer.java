2bdafb99aa0d71a62ec4325e73207d0c3aef0d1b
Your Name <you@example.com>
2016-11-26 17:04:51 +0800

    test

---
+++ APP-Scenes/src/com/app/empire/scene/SceneServer.java
@@ -0,0 +1,97 @@
+package com.app.empire.scene;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+import org.apache.log4j.Logger;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.executor.ExecutorFilter;
+import org.apache.mina.transport.socket.SocketSessionConfig;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.app.empire.protocol.Protocol;
+import com.app.empire.scene.service.ServiceManager;
+import com.app.empire.scene.session.ConnectSession;
+import com.app.empire.scene.session.SceneHandler;
+import com.app.net.ProtocolFactory;
+import com.app.protocol.data.DataBeanFilter;
+import com.app.protocol.s2s.S2SDecoder;
+import com.app.protocol.s2s.S2SEncoder;
+import com.app.session.Session;
+import com.app.session.SessionHandler;
+import com.app.session.SessionRegistry;
+
+public class SceneServer {
+	private static final Logger log = Logger.getLogger(SceneServer.class);
+
+	public static void main(String[] args) {
+		new SceneServer().launch();
+	}
+
+	private void launch() {
+		try {
+			ProtocolFactory.init(Protocol.class, "com.app.empire.protocol.data", "com.app.empire.scene.server.handler");
+			// ServiceManager serviceManager = ServiceManager.getManager();
+			ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+			ServiceManager sm = context.getBean(ServiceManager.class);
+			sm.initService();
+			ServiceManager.setServiceManager(sm);
+
+			openServerListener();
+			System.out.println("场景服务器启动!");
+		} catch (Exception e) {
+			log.error("server dispatcher launch failed!", e);
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 打开服务器监听
+	 * 
+	 * @throws IOException
+	 */
+	private void openServerListener() throws IOException {
+		// session注册
+		SessionRegistry registry = new SessionRegistry();
+		SessionHandler sessionHandler = new SceneSessionHandler(registry);
+
+		NioSocketAcceptor acceptor = new NioSocketAcceptor(Runtime.getRuntime().availableProcessors() + 1);
+		SocketSessionConfig cfg = acceptor.getSessionConfig();
+		cfg.setIdleTime(IdleStatus.BOTH_IDLE, 180);
+		// cfg.setReuseAddress(true);
+
+		// 添加IoHandler处理线程池
+		acceptor.getFilterChain().addFirst("uwap2databean", new DataBeanFilter());
+		acceptor.getFilterChain().addFirst("uwap2codec", new ProtocolCodecFilter(new S2SEncoder(), new S2SDecoder()));
+		acceptor.getFilterChain().addLast("threadPool", new ExecutorFilter(4, 16));
+		// 会话配置
+		cfg.setReceiveBufferSize(ServiceManager.getManager().getConfiguration().getInt("receivebuffsize"));
+		cfg.setSendBufferSize(ServiceManager.getManager().getConfiguration().getInt("writebuffsize"));
+		cfg.setTcpNoDelay(ServiceManager.getManager().getConfiguration().getBoolean("tcpnodelay"));
+		acceptor.setHandler(sessionHandler);
+		acceptor.setDefaultLocalAddress(new InetSocketAddress(ServiceManager.getManager().getConfiguration().getString("localip"), ServiceManager.getManager().getConfiguration().getInt("port")));
+		// 监听
+		acceptor.bind();
+	}
+
+	/**
+	 * 内部类 <code>DispatchSessionHandler</code>ip 分配器相关Handler
+	 * 
+	 * @see com.app.session.SessionHandler
+	 * @since JDK 1.6
+	 */
+	class SceneSessionHandler extends SceneHandler {
+		public SceneSessionHandler(SessionRegistry sessionRegistry) {
+			super(sessionRegistry);
+		}
+
+		public Session createSession(IoSession session) {
+			ConnectSession connSession = new ConnectSession(session);
+			return connSession;
+		}
+	}
+}
---
+++ APP-Scenes/src/com/app/empire/scene/session/ConnectSession.java
@@ -0,0 +1,110 @@
+package com.app.empire.scene.session;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.log4j.Logger;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+import com.app.empire.protocol.data.error.ProtocolError;
+import com.app.empire.scene.service.world.ArmyProxy;
+import com.app.protocol.data.AbstractData;
+import com.app.protocol.exception.ProtocolException;
+import com.app.session.Session;
+
+/**
+ * 类ConnectSession 客户sesson自定义封装应用，继承 Session 类
+ * 
+ * @see Session
+ * @author doter
+ */
+public class ConnectSession extends Session {
+	private static final Logger log = Logger.getLogger(ConnectSession.class);
+	private String name;
+
+	private ConcurrentHashMap<Integer, ArmyProxy> playerid2Army = new ConcurrentHashMap<Integer, ArmyProxy>();// 链接时
+
+	public ConnectSession(IoSession session) {
+		super(session);
+	}
+
+	@Override
+	public void created() {
+	}
+
+	@Override
+	public void closed() {
+		// 场景服务器关闭时的操作
+	}
+
+	@Override
+	public <T> void handle(T paramT) {
+
+	}
+
+	@Override
+	public void idle(IoSession session, IdleStatus status) {
+		System.out.println("关闭链接：" + session);
+		session.close(true);
+	}
+
+	@Override
+	public void opened() {
+
+	}
+
+	/**
+	 * 根据玩家id发送对应数据包
+	 * 
+	 * @param seg
+	 * @param playerId
+	 */
+	public void write(AbstractData seg, int playerId) {
+		ArmyProxy army = this.playerid2Army.get(playerId);
+		if (army != null) {
+			seg.setSessionId(army.getSessionId().intValue());
+			write(seg);
+		}
+	}
+
+	/**
+	 * 拋出异常
+	 */
+	@Override
+	public void sendError(ProtocolException ex) {
+		ProtocolError seg = new ProtocolError(ex);
+		write(seg);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * 游戏角色登录。
+	 * 
+	 * @param player
+	 * @param data
+	 * @param client
+	 * @param relogin
+	 * @return
+	 * @throws Exception
+	 */
+	public void playerLogin(ArmyProxy army) {
+		this.playerid2Army.put(army.getPlayer().getId(), army);
+	}
+
+	/**
+	 * 玩家退出
+	 * 
+	 * @param army
+	 */
+	public void playerLoginOut(ArmyProxy army) {
+		this.playerid2Army.remove(army.getPlayer().getId());
+	}
+
+}
